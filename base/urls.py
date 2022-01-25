62516fe092e4d5e24a1898ece88efa9700f95376
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-01-26 00:30:26 +0100

    templatki do katalogu aplikacji + pierwsza refraktoryzacja widoków

--- base/urls.py
+++ base/urls.py
@@ -22,5 +22,5 @@ from .import views
 urlpatterns = [
     path(),
     path('', views.home, name='home'),
-    path('room/', views.room, name='room'),
+    path('room/<str:pk>', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,10 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context=context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context=context)
 
