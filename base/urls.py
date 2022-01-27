f6819479b95ecef2faa4256737212b130486d318
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-01-27 15:21:49 +0100

    profile użytkownika + obsluga

--- base/urls.py
+++ base/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('room/<str:pk>', views.room, name='room'),
+    path('profile/<str:pk>', views.user_profile, name='user_profile'),
 
     path('create-room', views.create_room, name='create_room'),
     path('update-room/<str:pk>', views.update_room, name='update_room'),
