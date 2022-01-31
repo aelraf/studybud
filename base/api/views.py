2df40bb6f62c28991b8103c4cbb4d45d9f8b065c
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-01-31 23:53:06 +0100

    Django REST Framework - pierwszy widok API + zaktualizowane urls

--- base/api/views.py
+++ base/api/views.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from django.http import JsonResponse
+
+
+def get_route(request):
+    routes = [
+        'GET /api,',
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+    return JsonResponse(routes, safe=False)
