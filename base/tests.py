79911a87e74d1eba00bda9de8ccca7b7f6d1ecb0
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-21 16:40:34 +0100

    TestRoomView - metody setUp() oraz test_room_get()

--- base/tests.py
+++ base/tests.py
@@ -25,15 +25,21 @@ def create_room(
         name: str,
         topic: str,
         name_room: str = 'Pogadanki o Django',
-        desc: str = "Jesteśmy tutaj, aby pogadać o Django"
+        desc: str = "Jesteśmy tutaj, aby pogadać o Django",
+        participants=None,
 ) -> Room:
     host = User.objects.get(name=name)
     topic = Topic.objects.get(name=topic)
+    list_part = []
+    for part in participants:
+        list_part += part
+
     return Room.objects.create(
         host=host,
         topic=topic,
         name=name_room,
         description=desc,
+        participants=list_part,
     )
 
 
@@ -47,10 +53,9 @@ def create_message(user: str, room: str, message: str = 'Witaj') -> Message:
     )
 
 
-class TestHome(TestCase):
+class TestHomeView(TestCase):
     def setUp(self):
         create_user(name='Alex', email='alex@alex.com', bio='Jestem Alex')
-        # create_user(name='John', email='john@john.com', bio="Starzec")
         create_topics(name="Django")
         create_topics(name="Java")
         create_topics(name="C++")
@@ -97,3 +102,25 @@ class TestHome(TestCase):
         response_count = response.context['rooms'].count()
         self.assertEqual(response_count, 0)
 
+
+class TestRoomView(TestCase):
+    def setUp(self):
+        create_user(name='Alex', email='alex@alex.com', bio='Jestem Alex')
+        # create_user(name='John', email='john@john.com', bio="Starzec")
+        create_topics(name="Django")
+        create_topics(name="Java")
+        create_topics(name="C++")
+        create_room(name='Alex', topic="Django")
+        create_room(name='Alex', topic="Java", name_room="pogadanki o Javie", desc='tylko Java')
+        create_message(user='Alex', room="Pogadanki o Django")
+        create_message(user='Alex', room="Pogadanki o Django", message="to zaczynamy?")
+
+    def test_room_get(self):
+        pk = 1
+        response = self.client.get(reverse('room', kwargs={'pk': pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.context['room_messages'].count(), 2)
+
+    def test_room_post(self):
+        pass
